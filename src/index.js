69ec89bb52a20261ced0ff83d69b4e3fcc345791
Thomas Tuts <thomas@thomastuts.com>
2018-10-08 11:32:18 +0200

    Convert function component to class component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 
 const THE_MAGIC_NUMBER = 42;
 
-function Greeting() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-      <h2>This is a subtitle!</h2>
-      <p>2 + 2 equals {2 + 2}</p>
-      <p>The meaning of life is {THE_MAGIC_NUMBER}</p>
-    </div>
-  );
+class Greeting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <h2>This is a subtitle!</h2>
+        <p>2 + 2 equals {2 + 2}</p>
+        <p>The meaning of life is {THE_MAGIC_NUMBER}</p>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
