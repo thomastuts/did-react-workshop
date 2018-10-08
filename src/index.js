aa2045f25fe13c72f5ab342d7b8c78c56fb82c83
Thomas Tuts <thomas@thomastuts.com>
2018-10-08 18:00:58 +0200

    Add 'name' prop to Greeting component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const THE_MAGIC_NUMBER = 42;
 
 class Greeting extends React.Component {
+  static defaultProps = {
+    name: 'World',
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello world!</h1>
+        <h1>Hello {this.props.name}!</h1>
         <h2>This is a subtitle!</h2>
         <p>2 + 2 equals {2 + 2}</p>
         <p>The meaning of life is {THE_MAGIC_NUMBER}</p>
@@ -17,6 +26,6 @@ class Greeting extends React.Component {
 }
 
 ReactDOM.render(
-  <Greeting />,
+  <Greeting name="General Kenobi" />,
   document.getElementById('root')
 );
