c7072b933f1dee45b551a20e4bb983077e419e43
Thomas Tuts <thomas@thomastuts.com>
2018-10-08 11:31:27 +0200

    Add first basic functional component

--- src/index.js
+++ src/index.js
@@ -3,28 +3,18 @@ import ReactDOM from 'react-dom';
 
 const THE_MAGIC_NUMBER = 42;
 
-// Without JSX
-//ReactDOM.render(
-//  React.createElement(
-//    'div',
-//    {},
-//    React.createElement('h1', {}, 'Hello world!'),
-//    React.createElement('h2', {}, 'This is a subtitle!'),
-//    React.createElement('p', {}, `2 + 2 equals ${2 + 2}`),
-//    React.createElement('p', {}, `The meaning of life is ${THE_MAGIC_NUMBER}`),
-//  ),
-//  document.getElementById('root')
-//);
-
-// With JSX
-ReactDOM.render(
-  (
+function Greeting() {
+  return (
     <div>
       <h1>Hello world!</h1>
       <h2>This is a subtitle!</h2>
       <p>2 + 2 equals {2 + 2}</p>
       <p>The meaning of life is {THE_MAGIC_NUMBER}</p>
     </div>
-  ),
+  );
+}
+
+ReactDOM.render(
+  <Greeting />,
   document.getElementById('root')
 );
