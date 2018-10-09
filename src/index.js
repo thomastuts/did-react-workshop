f6749d717e9ca6d73628d43f03391e79cd54042f
Thomas Tuts <thomas@thomastuts.com>
2018-10-09 17:56:07 +0200

    Add filter to designer list to show all/alive/dead designers

--- src/index.js
+++ src/index.js
@@ -3,15 +3,58 @@ import React from 'react';
 
 import designers from './data/designers';
 
+class App extends React.Component {
+  state = {
+    filter: 'all',
+  };
+
+  handleChangeFilter = (e) => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  render() {
+    let filteredDesigners = designers;
+
+    switch (this.state.filter) {
+      case 'alive':
+        filteredDesigners = designers.filter(designer => !designer.deathYear);
+        break;
+      case 'dead':
+        filteredDesigners = designers.filter(designer => designer.deathYear);
+        break;
+      // no default
+    }
+
+    return (
+      <div>
+        {['all', 'alive', 'dead'].map(filter => (
+          <label key={filter}>
+            <input
+              type="radio"
+              name="filter"
+              value={filter}
+              checked={this.state.filter === filter}
+              onChange={this.handleChangeFilter}
+            />
+            {filter}
+          </label>
+        ))}
+
+        <ul>
+          {filteredDesigners.map(designer => (
+            <li key={designer.id} style={{ color: designer.favoriteColor }}>
+              {designer.name} ({designer.birthYear}{designer.deathYear && `-${designer.deathYear}`})
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  (
-    <ul>
-      {designers.map(designer => (
-        <li key={designer.id} style={{ color: designer.favoriteColor }}>
-          {designer.name} ({designer.birthYear}{designer.deathYear && `-${designer.deathYear}`})
-        </li>
-      ))}
-    </ul>
-  ),
+  <App />,
   document.getElementById('root')
 );
