0e6963d6d47ded885e10b5a499d735f53f9d1c1e
Thomas Tuts <thomas@thomastuts.com>
2018-10-10 23:11:02 +0200

    Add solution code with controlled components

--- src/index.js
+++ src/index.js
@@ -2,21 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${this.state.email}, password: ${this.state.password}`);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <div>
           <label>
             Email address:
             <br/>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password:
             <br/>
-            <input type="password" name="password" />
+            <input type="password" name="password" onChange={this.handleInputChange} />
           </label>
         </div>
         <button type="submit">Log in</button>
